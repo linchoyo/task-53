6afc7c33c73fd4856c216e51daa65ebdae1678e4
Lin Yo <linchoyo@gmail.com>
2022-12-20 11:36:25 +0200

    okay

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import "../scss/app.scss";
 
+let pv = document.querySelector(".price").textContent;
+let product = document.querySelector(".product");
+product.setAttribute("data-price", pv);
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
